1c9eeacf0ad148276eba122b488d69be16def672
Bethwelkip <bkiplimo@princeton.edu>
2020-09-07 07:18:24 -0400

    add toggle classes for the "what we do " page

--- js/app.js
+++ js/app.js
@@ -1,11 +1,49 @@
 $(document).ready(function () {
-    $().click(function () {
-
+    $("#what-hide1").click(function () {
+        $("#what-hide1").css("display", "none");
+        $(".photo1").css("display", "block");
     });
-    $().click(function () {
 
+    $("#photo1").click(function () {
+        $(".photo1").css("display", "none");
+        $("#what-hide1").css("display", "block");
     });
-    $().hover(function () {
+
+    $("#what-hide2").click(function () {
+        $("#what-hide2").css("display", "none");
+        $(".photo2").css("display", "block");
+    });
+
+    $("#photo2").click(function () {
+        $(".photo2").css("display", "none");
+        $("#what-hide2").css("display", "block");
+    });
+
+    $("#what-hide3").click(function () {
+        $("#what-hide3").css("display", "none");
+        $(".photo3").css("display", "block");
+    });
+
+    $("#photo3").click(function () {
+        $(".photo3").css("display", "none");
+        $("#what-hide3").css("display", "block");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+    $("design-photo", ".development-photo", ".pm-photo").click(function () {
+        $(".photo1", ".photo3", ".photo2").css("display", "block");
+    });
+    $(".card-img-top").hover(function () {
 
     });
     $().click(function () {
