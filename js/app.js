79ce7c1a7ea95d7728fdf8728e79ad824d93a464
Bethwelkip <bkiplimo@princeton.edu>
2020-09-07 11:11:39 -0400

    add  hover functions

--- js/app.js
+++ js/app.js
@@ -27,6 +27,10 @@ $(document).ready(function () {
     $("#photo3").click(function () {
         $(".photo3").css("display", "none");
         $("#what-hide3").css("display", "block");
+
+        $(".container").hover(function () {
+            $("body").removeClass(".hide");
+        })
     });
 
 
@@ -38,27 +42,4 @@ $(document).ready(function () {
 
 
 
-
-
-    $("design-photo", ".development-photo", ".pm-photo").click(function () {
-        $(".photo1", ".photo3", ".photo2").css("display", "block");
-    });
-    $(".card-img-top").hover(function () {
-
-    });
-    $().click(function () {
-
-    });
-
-
-    $("#blanks form").submit(function (event) {
-        var name = $("input#person1").val();
-        var email = $("input#person2").val();
-        var message = $("input#animal").val();
-        var toAlert = "Thank you for contacting us, " + name + ". We have received your message";
-
-        $("#story").show();
-
-        event.preventDefault();
-    });
 })
